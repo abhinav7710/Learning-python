b0b31dd95840a0813886b3092bc70216c6c7064f
abhinav7710 <abhinav2000id@gmail.com>
2026-08-12 22:54:46 +0530

    Learning to take input and trying one of its applications

---
+++ 2.py
@@ -0,0 +1,10 @@
+a = input("Enter a number")
+b=input("Enter another number")
+a = int(a)
+b= int(b)
+print("sum of numbers is:", a + b)
+
+#lets make it better
+a =int(input("Enter a number:"))
+b = int(input("Enter another number:"))
+print("sum of both numbers is:", a + b)
