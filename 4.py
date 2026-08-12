6b18327364de142a31253bc351c49af87763b0d2
abhinav7710 <abhinav2000id@gmail.com>
2026-08-12 23:39:38 +0530

    learning with arithemetic  operators

---
+++ 4.py
@@ -0,0 +1,9 @@
+a = int(input("enter a number:"))
+b = int(input("enter another number:"))
+        #arithemetic operators
+print("sum of both numbers is:", a + b)
+print("difference of both numbers is (first no - 2nd no):", a - b)
+print("product of both numbers is: ", a * b)
+print("division of both numbers gives quotient (as 1st no / 2nd no):", a/ b)
+print("division of both numbers gives remainder (as 1st no / 2nd no):", a % b)
+print("quotient in whole number division (as 1st no // 2nd no):", a // b)
