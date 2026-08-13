463a8ca32452a9a760156d996d7fcc1cfd5dd935
abhinav7710 <abhinav2000id@gmail.com>
2026-08-13 23:03:01 +0530

    Learning continue and pass statements

---
+++ 17.py
@@ -0,0 +1,16 @@
+#learning continue and pass statements
+
+print("trying continue statements")
+for i in range(0, 5):
+    if i == 2:
+     continue
+
+    print(i)
+
+
+print("trying pass statements")
+for i in range(0,5):
+   if i==3:
+     pass
+   print(i)
+
