6cf348c9c975a70526bb177f087c6304568b56c5
abhinav7710 <abhinav2000id@gmail.com>
2026-08-13 23:58:44 +0530

    Program for giving  output  as day according to the number given by user in (1-7

---
+++ 21.py
@@ -0,0 +1,20 @@
+#giving day output according to the number given by user in (1-7)
+a = int(input("Enter a number (1-7): "))
+
+match a:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Invalid input")
