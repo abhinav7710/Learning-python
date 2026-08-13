387eff416f556f7c8db267fa86b5298a6156ed19
abhinav7710 <abhinav2000id@gmail.com>
2026-08-13 23:14:24 +0530

    A  program that tells the user that given number as input is positive, negative or zero

---
+++ 18.py
@@ -0,0 +1,9 @@
+#writing a program that tells the user that given number as input is positive, negative or zero
+a = int(input("Enter an integer: "))
+if a > 0:
+    print("The number is a positive integer")
+
+elif  a == 0:
+    print("The given integer is zero")
+else:
+    print("The given integer is negative")        
