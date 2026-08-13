906049fc9a9b4746cb0125321fca6c452c1ea757
abhinav7710 <abhinav2000id@gmail.com>
2026-08-13 10:25:14 +0530

    Learning assignment operators

---
+++ 6.py
@@ -0,0 +1,24 @@
+#assignment operators
+a = 32
+print(a)
+print(type(a))
+a*=3
+print(a)
+print(type(a))
+
+b = 45
+print(b)
+b/=5
+print(b)
+
+c = 10
+print(c)
+c+=8
+print(c)
+
+d=18
+print(d)
+d -=7
+print(d)
+
+
