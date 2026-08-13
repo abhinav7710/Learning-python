0962e0dec5125b719607301dc512b3d0d32b9f96
abhinav7710 <abhinav2000id@gmail.com>
2026-08-13 10:53:37 +0530

    Tying a basic printing question

---
+++ 8.py
@@ -0,0 +1,8 @@
+Name="abhinav"
+age = 18
+height=185.42
+is_student = True
+
+print("name is: " + Name + ". age is " + str(age) + ". height is " + str(height) + ". is a student:  " + str(is_student))
+
+      
