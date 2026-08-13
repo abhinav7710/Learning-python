de22628fe0041d94ecbe6c24cffd1369c38f8694
abhinav7710 <abhinav2000id@gmail.com>
2026-08-13 23:47:39 +0530

    Program to check voting eligibility

---
+++ 19.py
@@ -0,0 +1,7 @@
+#writing a program to check voting eligibility of user
+age = int(input("Enter your age: "))
+if age >= 18:
+ print("You are an eligible voter")
+
+else:
+  print("you can't vote because you are a minor")
