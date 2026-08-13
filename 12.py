e35774e9fdd0b82528c64d9a1f39e4b1bce15b29
abhinav7710 <abhinav2000id@gmail.com>
2026-08-13 11:33:42 +0530

    Learning if-else conditional statements

--- 12.py
+++ 12.py
@@ -1,6 +1,8 @@
 #Learning if-else conditional statements
 age= input("enter your age: ")
-if int(age) >=18:
+if int(age) >18:
     print("you can drive")
+elif int(age) == 18:
+    print("lets take a quick driving test")
 else:
-    print("you can't drive")
+   print("you can't drive")
