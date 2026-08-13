22ac49520dc9dbce38805e3dec20062b6372b093
abhinav7710 <abhinav2000id@gmail.com>
2026-08-13 11:13:18 +0530

    Taking integer input and giving square and cube value of the number as output

---
+++ 11.py
@@ -0,0 +1,4 @@
+#taking a number from user and printing its square and cube
+num= int(input("Enter a number: "))
+print("Square of the number is: " + str(num * num))
+print("cube of the number is: " + str(num * num * num))
