7f44d944c6fc00d60658a8bdeecd428e834842a9
abhinav7710 <abhinav2000id@gmail.com>
2026-08-13 11:05:08 +0530

    Trying a simple printing question

---
+++ 10.py
@@ -0,0 +1,4 @@
+#asking user about his favourite food and telling that I like it too
+food= input("What is your favourite food? ")
+print("Oh what a coincidence, It is my favourite food too and I really love " + str(food))
+            
