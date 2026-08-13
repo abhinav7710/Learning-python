bc84701af66af098bf6d36d2398bc8fe5ec8b6f6
abhinav7710 <abhinav2000id@gmail.com>
2026-08-13 23:52:08 +0530

    Program to check number given by user is even or odd

---
+++ 20.py
@@ -0,0 +1,7 @@
+#writing a program to determine that number given by user is even or odd
+a = int (input("Enter a number: "))
+if a % 2 == 0:
+    print("The number is even")         
+
+else:
+    print("The number is odd")
