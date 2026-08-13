6f53d2c93b9bbea373df06b2d30ae451f625c394
abhinav7710 <abhinav2000id@gmail.com>
2026-08-13 10:18:07 +0530

    learning logical operators

---
+++ 5.py
@@ -0,0 +1,16 @@
+#logical operators
+print("and operators")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print("or operators")
+print(True or False)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print("not operators")
+print(not True)
+print(not False)
