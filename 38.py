87ad56a0d80e879ebb970e249503a914f82f601c
abhinav7710 <abhinav2000id@gmail.com>
2026-08-14 20:25:45 +0530

    Trying a basic question on string manipulation changes

---
+++ 38.py
@@ -0,0 +1,10 @@
+#Trying a basic question on string manipulation changes
+print('Q1.Given sentence = "Coding in Python is fun" , replace "fun" with "awesome" and print it')
+text= "Coding in Python is fun"
+print(text.replace("fun", "awesome"))
+
+print("Find the index of the word 'Python' in sentence")
+print(text.find("Python"))
+
+print("Convert the entire sentence to uppercase and print it")
+print(text.upper())
