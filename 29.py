d5d9fbb818d476900cd950c8a0aae367955949f5
abhinav7710 <abhinav2000id@gmail.com>
2026-08-14 14:17:25 +0530

    Learning ways to write a string

---
+++ 29.py
@@ -0,0 +1,11 @@
+#Learning ways to write a string
+print("Single quotd string")
+a = 'Lionel Messi is the greatest of all time'
+print(str(a))
+print("double quoted string")
+b = "Lionel Messi is the greatest of all time"
+print(str(b))
+print("multi line string")
+c = '''Lionel Messi is the greatest of ll time and heisa magician with the ball. 
+He is so good that the greatest stricker of all time CristiaoRonaldo's biggest achievement is to be compared to him.'''
+print(str(c))
