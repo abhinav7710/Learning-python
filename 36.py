2c4e8010c4cb2afbccb63572c3bca403d82430b0
abhinav7710 <abhinav2000id@gmail.com>
2026-08-14 20:10:44 +0530

    Trying basic questions based  on string methods and functions

---
+++ 36.py
@@ -0,0 +1,16 @@
+#Trying questions based  on string methods and functions
+print("Take the string ' i love python programming '")
+text= ' i love python programming '
+print("Remove extra spaces from both ends")
+print(text.strip())
+
+print("Convert it to title case")
+print(text.title())
+
+print("Count how many times 'o' appears")
+print(text.count("o"))
+
+
+print("Check if the string '123abc' is alphanumeric")
+a= "123abc"
+print(a.isalnum())
