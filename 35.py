6bcdba13652f37d14402cc5c74ec357f695af74f
abhinav7710 <abhinav2000id@gmail.com>
2026-08-14 18:20:28 +0530

    Trying a qutionbased on string operations

---
+++ 35.py
@@ -0,0 +1,15 @@
+#Trying a qutionbased on string operations
+print("1.question on string slicing and indexing")
+print("Given text = "python programming" ,do the following:")
+print("Print the first 6 characters")
+print("Print the last 6 characters")
+
+text= "python programming"
+print(text[0:7])
+print(text[12:])
+
+print("q2.every second charecter from the string")
+print(text[::2])
+print('q3. reverse the string')
+print(text[::-1])
+
