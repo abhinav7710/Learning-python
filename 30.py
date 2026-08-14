df696ba979c527c0af7f5861e258678e89995e37
abhinav7710 <abhinav2000id@gmail.com>
2026-08-14 14:28:47 +0530

    Learning indexing of a string

---
+++ 30.py
@@ -0,0 +1,20 @@
+#Learning indexing of a string
+name = "ABHINAV"
+#with positive indexing
+print(name[0]) 
+print(name[1]) 
+print(name[2]) 
+print(name[3]) 
+print(name[4]) 
+print(name[5]) 
+print(name[6]) 
+
+#with negative indexing
+
+print(name[-7]) 
+print(name[-6]) 
+print(name[-5]) 
+print(name[-4]) 
+print(name[-3]) 
+print(name[-2]) 
+print(name[-1])
