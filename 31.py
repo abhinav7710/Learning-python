df1530800a001dbaa059fdf3f0f5847a61b0568f
abhinav7710 <abhinav2000id@gmail.com>
2026-08-14 16:14:40 +0530

    Learning string slicing

--- 31.py
+++ 31.py
@@ -7,3 +7,14 @@ print(name[:18])
 print(name[7:])
 
 print(name[::2])
+
+print(name[-4:5])
+
+print("step parameter")
+print(name[::3])
+print(name[::-1])
+
+print("practical uses")
+print(name[:8])
+print(name[-7:])
+print(name[1:-5])
