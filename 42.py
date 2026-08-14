c7a108e04e22b3c05c415bd74daf929fc0b6ae36
abhinav7710 <abhinav2000id@gmail.com>
2026-08-14 22:35:25 +0530

    Learning to assign the value of function output to a variable

---
+++ 42.py
@@ -0,0 +1,10 @@
+#Learning to assign the value of function output to a variable
+def average(a,b,c):
+    d = (a + b + c)
+    return d #print d is not being used
+
+O1 = average(68,93,29764)
+O2 = average(9835,7283,7193963)
+
+print(O1)
+print(O2)
