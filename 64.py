77817c332cfc3b341c881fa955d1d98d56420e82
abhinav7710 <abhinav2000id@gmail.com>
2026-08-16 04:47:07 +0530

    Learning list comprehensions (efficient list creation)

---
+++ 64.py
@@ -0,0 +1,7 @@
+#Learning list comprehensions (efficient list creation)
+squared=[x**2 for x in range(5)]
+print(squared)
+
+#Make a list that holds the table of 7 in it
+table=[5*i for i in range(1,11)]
+print(table)
