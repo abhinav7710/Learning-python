31cd70c0157de40bf12755d846ea000f4471ad1a
abhinav7710 <abhinav2000id@gmail.com>
2026-08-15 22:55:49 +0530

    Practicing a question based on docstrings

---
+++ 57.py
@@ -0,0 +1,17 @@
+#Practicing a question based on docstrings
+'''Write a function multiply(a, b) that has a proper docstring explaining what
+it does. Then use help(multiply) to display the docstring'''
+
+def multiply(a, b):
+    '''Returns the product of two numbers
+    
+    Parameters:
+    a(int):The first number
+    b(int): The second number
+    
+    Returns:
+    int: The product of the two numbers
+    '''
+    return a*b
+print(multiply(5,4))
+print(help(multiply))
