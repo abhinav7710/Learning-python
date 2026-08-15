d8e6fc3a5c208dc95283d621f36a17eb122664f1
abhinav7710 <abhinav2000id@gmail.com>
2026-08-16 05:07:06 +0530

    Learning tuples and operations on tuples

---
+++ 65.py
@@ -0,0 +1,16 @@
+#Learning tuples and operations on tuples
+#creating a tuple
+a1= (18,77,91,560)
+single_element= (5, )
+
+#accessing tuple elements
+print(a1[2])
+
+print(a1.count(77))
+print(a1.index(560))
+
+
+#Unpacking of tuple
+tuple= (3, 2, 776)
+a, b, c =tuple
+print(a,b,c) 
