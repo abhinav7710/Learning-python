a25d9bf39d2448bbd93155aa931df9f06882e479
abhinav7710 <abhinav2000id@gmail.com>
2026-08-15 20:59:48 +0530

    Trying some basic questions based on function

---
+++ 50.py
@@ -0,0 +1,14 @@
+#Trying some basic questions based on function
+print("Write a function greet() that prints 'Hello, Python Learner' when called")
+def greet():
+    print("Hello, Python Learner")
+
+greet()
+
+print("Write a function square(num) that returns the square of a given number")
+def square(a):
+    d= a*a
+    return d
+
+
+print(square(7))
