200cf110e6dc5d321f898dc4274fb9a5ca5e5ebf
abhinav7710 <abhinav2000id@gmail.com>
2026-08-16 20:32:20 +0530

    trying some basic questions based on lists

--- 69.py
+++ 69.py
@@ -14,7 +14,7 @@ print(fruits)
 print(len(fruits))
 
 #Create a list of numbers from 1 to 10 .
-a= ["1", "2","3", "4", "5", "6", "7", "8", "9", "10"]
+a= [i for i in range(1,11)]
 
 #Print the first three numbers using slicing.
 print(a[0:3])
