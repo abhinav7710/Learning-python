faacaba958a71f3170dd22cbae23d8ab1db2152d
abhinav7710 <abhinav2000id@gmail.com>
2026-08-16 19:44:22 +0530

    Trying some basic questions based on list methods

---
+++ 70.py
@@ -0,0 +1,13 @@
+#Trying some basic questions based on list methods
+'''Start with numbers = [5, 2, 9, 1, 7] and do the following:
+Sort the list in ascending order.
+Append the number 10 to the list.
+Remove the number 2 from the list'''
+
+numbers =[5, 2, 9, 1, 7]
+numbers.sort()
+print(numbers)
+numbers.append(10)
+print(numbers)      
+numbers.remove(2)
+print(numbers)
