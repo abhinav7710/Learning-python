0fa026483340349145612eccf53c91b91451a75d
abhinav7710 <abhinav2000id@gmail.com>
2026-08-16 20:34:06 +0530

    Trying some questions based on sets and set methods

---
+++ 73.py
@@ -0,0 +1,11 @@
+#Trying some questions based on sets and set methods
+#Create a set my_set = {1, 2, 3, 3, 4} and print it. Add 5 to the set, remove 2
+
+my_set= {1,2,3,4}
+print(my_set)
+
+my_set.add(5)
+print(my_set)
+
+my_set.remove(2)
+print(my_set)
