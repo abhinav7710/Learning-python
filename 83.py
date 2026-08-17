c797eb2dfc83fd0c723f4c6383df357c23d10f3f
abhinav7710 <abhinav2000id@gmail.com>
2026-08-17 23:00:43 +0530

    Creating a class car with a method drive() that prints "Car is moving" . Also creating an object of Car and calling drive() .

---
+++ 83.py
@@ -0,0 +1,6 @@
+#Create a class Car with a method drive() that prints "Car is moving" . Create an object of Car and call drive() .
+class car:
+    def drive(self):
+        return ("Car is moving")
+c1 = car()
+print(c1.drive())        
