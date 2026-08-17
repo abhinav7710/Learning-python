61c81b2470560d4ffdb2d87bbf859b8075436cca
abhinav7710 <abhinav2000id@gmail.com>
2026-08-17 21:32:39 +0530

    Learning overriding: customized inherited behaviour

---
+++ 82.py
@@ -0,0 +1,18 @@
+#Learning overriding: customized inherited behaviour
+class point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y= y
+    def sum(self, p):
+        return point((self.x + p.x), (self.y +p.y))
+    def print_point(self):
+        return f"X is {self.x} and y is {self.y}"
+    def __add__(self, p):
+        return point((self.x + p.x), (self.y +p.y))
+
+p1= point(3,2)
+p2= point (6,3)
+
+p= p1.sum(p2) #returns a new point whih is sum of p1 and p2
+p= p1 + p2 #overloading add operator by writing __add__ function
+print(p.print_point())
