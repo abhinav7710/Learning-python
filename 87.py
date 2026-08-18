cddc5030b98dee8eebce4d2130f1e6dd71d303ed
abhinav7710 <abhinav2000id@gmail.com>
2026-08-18 21:24:12 +0530

    Learning decorator with arguments

---
+++ 87.py
@@ -0,0 +1,12 @@
+#Learning decorator with arguments
+def repeat(n):
+    def decorator(func):
+        def wrapper(a):
+            for i in range(n):
+                func(a)
+        return wrapper
+    return decorator
+@repeat(3)
+def greet(name):
+    print(f"Hello, {name}")
+greet("abhinav")            
